d8955dbeaff192844356c11f6ebde3327f0777f8
MD Enamul Haque <eng.enamulcse@gmail.com>
2023-02-25 09:03:17 +0600

    date 2023

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const canvasC = document.getElementById('c');
 
 const config = {
   birthdate: 'Feb 26, 2023',
-  name: 'Khadija Aktar'
+  name: 'Khadija'
 };
 
 function hideEverything() {
@@ -48,7 +48,7 @@ x = setInterval(function() {
     hw = w / 2, // half-width
     hh = h / 2,
     opts = {
-      strings: ['HAPPY', 'BIRTHDAY!','MY LOVE', config.name],
+      strings: ['HAPPY', 'BIRTHDAY!', config.name],
       charSize: 30,
       charSpacing: 35,
       lineHeight: 40,
